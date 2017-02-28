2f56b9be0c49ec7c0ee94f56b10d8a34091bb7f9
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-27 18:44:45 -0600

    First commit

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+function puke(object) {
+    return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+function ConfirmBattle(props) {
+    return props.isLoading === true
+        ? <p>LOADING</p>
+        : <p>CONFIRM BATTLE: {puke(props)}</p>
+}
+
+module.exports = ConfirmBattle;
---
+++ app/components/Guest.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Guest = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello from Guest
+            </div>
+        )
+    }
+});
+
+module.exports = Guest;
