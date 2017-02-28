3908dd392e6a3892d929092c3183a03f03da536b
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-27 23:48:54 -0600

    Battle functionality and results route added

---
+++ app/components/Results.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function puke (obj) {
+  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
+function Results (props) {
+  return <div>{puke(props)}</div>
+}
+
+Results.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired,
+  scores: PropTypes.array.isRequired
+
+}
+
+module.exports = Results;
